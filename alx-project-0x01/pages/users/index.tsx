e27149538cf769e0708526a4f0d3023fc94731d8
d-madiou <madioudiallo846@gmail.com>
2025-05-31 01:17:35 +0800

    The typescript

---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/layout/Header";
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+<Header/>
